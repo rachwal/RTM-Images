02cbbed13775bbd792c6194ce48853b3bd308709
Bartosz Rachwał <bartosz.rachwal@gmail.com>
2015-08-06 15:31:42 -0700

    Update

---
+++ RTM.Images.Decoder/IImagesDecoder.cs
@@ -0,0 +1,14 @@
+﻿// RTM.Images
+// RTM.Images.Decoder
+// IImagesDecoder.cs
+// 
+// Created by Bartosz Rachwal. 
+// Copyright (c) 2015 The National Institute of Advanced Industrial Science and Technology, Japan. All rights reserved. 
+
+namespace RTM.Images.Decoder
+{
+    public interface IImagesDecoder<out T>
+    {
+        T Decode(string encodedPicture);
+    }
+}
---
+++ RTM.Images.Provider/IImageProvider.cs
@@ -0,0 +1,17 @@
+﻿// RTM.Images
+// RTM.Images.Provider
+// IImageProvider.cs
+// 
+// Created by Bartosz Rachwal. 
+// Copyright (c) 2015 The National Institute of Advanced Industrial Science and Technology, Japan. All rights reserved. 
+
+using System;
+
+namespace RTM.Images.Provider
+{
+    public interface IImageProvider
+    {
+        string EncodedImage { get; set; }
+        event EventHandler NewImage;
+    }
+}
