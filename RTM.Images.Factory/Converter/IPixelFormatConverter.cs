d7b36cd8acae7609db78eb6efea9207a76a2661a
Bartosz Rachwał <bartosz.rachwal@gmail.com>
2015-08-10 09:06:45 -0700

    Update

---
+++ RTM.Images.Factory/Converter/IPixelFormatConverter.cs
@@ -0,0 +1,18 @@
+﻿// RTM.Images
+// RTM.Images.Factory
+// IPixelFormatConverter.cs
+// 
+// Created by Bartosz Rachwal. 
+// Copyright (c) 2015 The National Institute of Advanced Industrial Science and Technology, Japan. All rights reserved. 
+
+using System.Drawing.Imaging;
+
+namespace RTM.Images.Factory.Converter
+{
+    public interface IPixelFormatConverter
+    {
+        PixelFormat Convert(System.Windows.Media.PixelFormat pixelFormat);
+        PixelFormat Convert(string pixelFormat);
+        System.Windows.Media.PixelFormat Convert(PixelFormat pixelFormat);
+    }
+}
--- RTM.Images.Factory/Converter/PixelFormatConverter.cs
+++ RTM.Images.Factory/Converter/PixelFormatConverter.cs
@@ -5,141 +5,159 @@
 // Created by Bartosz Rachwal. 
 // Copyright (c) 2015 The National Institute of Advanced Industrial Science and Technology, Japan. All rights reserved. 
 
+using System;
 using System.Drawing.Imaging;
 
 namespace RTM.Images.Factory.Converter
 {
-    public interface IPixelFormatConverter
-    {
-        System.Drawing.Imaging.PixelFormat Convert(System.Windows.Media.PixelFormat pixelFormat);
-        System.Drawing.Imaging.PixelFormat Convert(string pixelFormat);
-        System.Windows.Media.PixelFormat Convert(System.Drawing.Imaging.PixelFormat pixelFormat);
-    }
-
     public class PixelFormatConverter : IPixelFormatConverter
     {
-        public System.Drawing.Imaging.PixelFormat Convert(System.Windows.Media.PixelFormat pixelFormat)
+        public PixelFormat Convert(System.Windows.Media.PixelFormat pixelFormat)
         {
             if (pixelFormat == System.Windows.Media.PixelFormats.Indexed1)
             {
-                return System.Drawing.Imaging.PixelFormat.Format1bppIndexed;
+                return PixelFormat.Format1bppIndexed;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Indexed4)
             {
-                return System.Drawing.Imaging.PixelFormat.Format4bppIndexed;
+                return PixelFormat.Format4bppIndexed;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Indexed8)
             {
-                return System.Drawing.Imaging.PixelFormat.Format8bppIndexed;
+                return PixelFormat.Format8bppIndexed;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.BlackWhite)
             {
-                return System.Drawing.Imaging.PixelFormat.Format1bppIndexed;
+                return PixelFormat.Format1bppIndexed;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Gray4)
             {
-                return System.Drawing.Imaging.PixelFormat.Format4bppIndexed;
+                return PixelFormat.Format4bppIndexed;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Gray8)
             {
-                return System.Drawing.Imaging.PixelFormat.Format8bppIndexed;
+                return PixelFormat.Format8bppIndexed;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Rgb24)
             {
-                return System.Drawing.Imaging.PixelFormat.Format24bppRgb;
+                return PixelFormat.Format24bppRgb;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Rgb48)
             {
-                return System.Drawing.Imaging.PixelFormat.Format48bppRgb;
+                return PixelFormat.Format48bppRgb;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Rgba64)
             {
-                return System.Drawing.Imaging.PixelFormat.Format64bppArgb;
+                return PixelFormat.Format64bppArgb;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Prgba64)
             {
-                return System.Drawing.Imaging.PixelFormat.Format64bppPArgb;
+                return PixelFormat.Format64bppPArgb;
             }
             if (pixelFormat == System.Windows.Media.PixelFormats.Gray16)
             {
-                return System.Drawing.Imaging.PixelFormat.Format16bppGrayScale;
+                return PixelFormat.Format16bppGrayScale;
             }
             if (pixelFormat.BitsPerPixel == System.Windows.Media.PixelFormats.Gray8.BitsPerPixel)
             {
-                return System.Drawing.Imaging.PixelFormat.Format8bppIndexed;
+                return PixelFormat.Format8bppIndexed;
             }
             if (pixelFormat.BitsPerPixel == System.Windows.Media.PixelFormats.Gray16.BitsPerPixel)
             {
-                return System.Drawing.Imaging.PixelFormat.Format16bppGrayScale;
+                return PixelFormat.Format16bppGrayScale;
             }
             if (pixelFormat.BitsPerPixel == System.Windows.Media.PixelFormats.Rgb24.BitsPerPixel)
             {
-                return System.Drawing.Imaging.PixelFormat.Format24bppRgb;
+                return PixelFormat.Format24bppRgb;
             }
             if (pixelFormat.BitsPerPixel == System.Windows.Media.PixelFormats.Rgb48.BitsPerPixel)
             {
-                return System.Drawing.Imaging.PixelFormat.Format48bppRgb;
+                return PixelFormat.Format48bppRgb;
             }
             if (pixelFormat.BitsPerPixel == System.Windows.Media.PixelFormats.Rgba64.BitsPerPixel)
             {
-                return System.Drawing.Imaging.PixelFormat.Format64bppArgb;
+                return PixelFormat.Format64bppArgb;
             }
-            return System.Drawing.Imaging.PixelFormat.DontCare;
+            return PixelFormat.DontCare;
         }
 
-        public PixelFormat Convert(string pixelFormat)
+        public System.Windows.Media.PixelFormat Convert(PixelFormat pixelFormat)
         {
-            return System.Drawing.Imaging.PixelFormat.DontCare;
-        }
-
-        public System.Windows.Media.PixelFormat Convert(System.Drawing.Imaging.PixelFormat pixelFormat)
-        {
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format1bppIndexed)
+            if (pixelFormat == PixelFormat.Format1bppIndexed)
             {
                 return System.Windows.Media.PixelFormats.Indexed1;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format4bppIndexed)
+            if (pixelFormat == PixelFormat.Format4bppIndexed)
             {
                 return System.Windows.Media.PixelFormats.Indexed4;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format8bppIndexed)
+            if (pixelFormat == PixelFormat.Format8bppIndexed)
             {
                 return System.Windows.Media.PixelFormats.Indexed8;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format1bppIndexed)
+            if (pixelFormat == PixelFormat.Format1bppIndexed)
             {
                 return System.Windows.Media.PixelFormats.BlackWhite;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format4bppIndexed)
+            if (pixelFormat == PixelFormat.Format4bppIndexed)
             {
                 return System.Windows.Media.PixelFormats.Gray4;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format8bppIndexed)
+            if (pixelFormat == PixelFormat.Format8bppIndexed)
             {
                 return System.Windows.Media.PixelFormats.Gray8;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format24bppRgb)
+            if (pixelFormat == PixelFormat.Format24bppRgb)
             {
                 return System.Windows.Media.PixelFormats.Rgb24;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format48bppRgb)
+            if (pixelFormat == PixelFormat.Format48bppRgb)
             {
                 return System.Windows.Media.PixelFormats.Rgb48;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format64bppArgb)
+            if (pixelFormat == PixelFormat.Format64bppArgb)
             {
                 return System.Windows.Media.PixelFormats.Rgba64;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format64bppPArgb)
+            if (pixelFormat == PixelFormat.Format64bppPArgb)
             {
                 return System.Windows.Media.PixelFormats.Prgba64;
             }
-            if (pixelFormat == System.Drawing.Imaging.PixelFormat.Format16bppGrayScale)
+            if (pixelFormat == PixelFormat.Format16bppGrayScale)
             {
                 return System.Windows.Media.PixelFormats.Gray16;
             }
 
             return System.Windows.Media.PixelFormats.Default;
+        }
+
+        public PixelFormat Convert(string pixelFormat)
+        {
+            return TryParse(pixelFormat);
+        }
+
+        private PixelFormat TryParse(string pixelFormat)
+        {
+            try
+            {
+                var format = (PixelFormat) Enum.Parse(typeof (PixelFormat), pixelFormat, true);
+                return format;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                var format =
+                    (System.Windows.Media.PixelFormat)
+                        Enum.Parse(typeof (System.Windows.Media.PixelFormat), pixelFormat, true);
+                return Convert(format);
+            }
+            catch (Exception)
+            {
+                return Convert(System.Windows.Media.PixelFormats.Default);
+            }
         }
     }
 }
